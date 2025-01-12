24a7e13ccab841cf3d5a89b3340a1a26e387a721
Shipan Mallik <mshipan657@gmail.com>
2025-01-12 16:57:36 +0600

    main layout design changes from main.jsx file

--- velki/src/main.jsx
+++ velki/src/main.jsx
@@ -6,24 +6,6 @@ import router from "./routes/Router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div
-      style={{
-        backgroundImage:
-          "url('https://www.wickspin24.live/images/velki-desktop-bg.webp')",
-        backgroundSize: "cover",
-      }}
-      className="h-screen flex justify-center"
-    >
-      <div className="hidden lg:flex w-[30%] md:w-[20%] items-center justify-center">
-        <img
-          src="https://www.wickspin24.live/images/velki-logo-desktop.webp"
-          alt=""
-        />
-      </div>
-      <div className="w-full md:w-[60%] lg:w-[40%] xl:w-[30%] bg-[#eef6fb] overflow-y-auto">
-        <RouterProvider router={router} />
-      </div>
-      <div className="hidden lg:block md:w-[20%] w-[30%]"></div>
-    </div>
+    <RouterProvider router={router} />
   </StrictMode>
 );
